5cf9f35e2e41a2d3a88c485a7f032d9183380644
greatlaki <greatlaki911@gmail.com>
2022-08-18 00:06:40 +0300

    model was registered

--- todoapp/todo/admin.py
+++ todoapp/todo/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'date', 'done')
+    list_display_links = ('id', 'title')
+    search_fields = ('id', 'title', 'description')
+    list_editable = ('done',)
+
+
+admin.site.register(Todo, TodoAdmin)
